abaedbfab3704db4f1593fbda6c9abdcb3a767e4
Enmanuel Balcacer <makuttico@gmail.com>
2021-04-25 00:35:45 -0400

    Adding Response class

---
+++ DTO/Response.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace kontacto_api.DTO
+{
+    public class Response
+    {
+        public string Message { get; set; }
+        public Object Data { get; set; }
+    }
+}
