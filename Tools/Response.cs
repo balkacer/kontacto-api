ea91b6827fbe186ac93a0e752607e9b22301f07f
Enmanuel Balcacer <makuttico@gmail.com>
2021-04-25 02:43:12 -0400

    Moving Response class to the Tools directory and setting constructors

---
+++ Tools/Response.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using kontacto_api.Tools.Enums;
+
+namespace kontacto_api.Tools
+{
+
+    public class Response
+    {
+        public Response( string Message, ResponseCodeEnum Code )
+        {
+            this.Message = Message;
+            this.Code = Code;
+        }
+
+        public Response( string Message, ResponseCodeEnum Code, object Data )
+        {
+            this.Message = Message;
+            this.Code = Code;
+            this.Data = Data;
+        }
+
+        public string Message { get; set; }
+        public ResponseCodeEnum Code { get; set; }
+        #nullable enable
+        public object? Data { get; set; }
+    }
+}
