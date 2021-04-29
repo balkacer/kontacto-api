2baa796fcc24b2c2eaca0822199c6bfe21625671
Enmanuel Balcacer <makuttico@gmail.com>
2021-04-29 03:05:04 -0400

    Formating AutService code

--- Services/AuthService.cs
+++ Services/AuthService.cs
@@ -18,7 +18,8 @@ namespace kontacto_api.Services
         {
             _context = context;
         }
-        private async Task<GetPrivateUserDTO> GetPrivateUserDTOAsync(string id) {
+        private async Task<GetPrivateUserDTO> GetPrivateUserDTOAsync(string id)
+        {
             var user = await _context.Users
                 .Include(s => s.UserStatus)
                 .Include(t => t.UserType)
@@ -32,7 +33,8 @@ namespace kontacto_api.Services
 
             var pUser = await _context.PrivateUsers.FindAsync(id);
 
-            var addresObj = new AddressDTO {
+            var addresObj = new AddressDTO
+            {
                 Address = user.Address.Address1,
                 SecondAddress = user.Address.SecondAddress,
                 Latitude = user.Address.Latitude,
@@ -42,7 +44,8 @@ namespace kontacto_api.Services
                 CountryCode = city.Country.Code
             };
 
-            var pUserDTO = new GetPrivateUserDTO {
+            var pUserDTO = new GetPrivateUserDTO
+            {
                 Id = user.Id,
                 FirstName = pUser.FirstName,
                 SecondName = pUser.SecondName,
@@ -63,8 +66,8 @@ namespace kontacto_api.Services
 
             return pUserDTO;
         }
-        private async Task<GetBusinessUserDTO> GetBusinessUserDTOAsync(string id) {
-
+        private async Task<GetBusinessUserDTO> GetBusinessUserDTOAsync(string id)
+        {
             var user = await _context.Users
                 .Include(s => s.UserStatus)
                 .Include(t => t.UserType)
@@ -88,7 +91,8 @@ namespace kontacto_api.Services
                 CountryCode = city.Country.Code
             };
 
-            var bUserDTO = new GetBusinessUserDTO {
+            var bUserDTO = new GetBusinessUserDTO
+            {
                 Id = user.Id,
                 Name = bUser.Name,
                 AnniversaryDate = bUser.AnniversaryDate.ToString("yyyy/MM/dd"),
@@ -103,7 +107,8 @@ namespace kontacto_api.Services
 
             return bUserDTO;
         }
-        public async Task<object> GetUserAsync(string id) {
+        public async Task<object> GetUserAsync(string id)
+        {
             var userType = await this.GetUserTypeAsync(id);
 
             if (userType == null) return null;
@@ -112,23 +117,22 @@ namespace kontacto_api.Services
                 var pUser = await this.GetPrivateUserDTOAsync(id);
                 return pUser;
             }
-
-            var bUser = await this.GetBusinessUserDTOAsync(id);
-            return bUser;
-        }
-        private async Task<string> GetUserTypeAsync(string id) {
+        private async Task<string> GetUserTypeAsync(string id)
+        {
             var user = await _context.Users.FindAsync(id);
             if (user == null) return null;
 
             var userType = await _context.UserTypes.FindAsync(user.UserTypeId);
             return userType.Type;
         }
-        private async Task<User> CreateNewUserAsync(User user) {
+        private async Task<User> CreateNewUserAsync(User user)
+        {
             await _context.Users.AddAsync(user);
             await _context.SaveChangesAsync();
             return user;
         }
-        public async Task<Response<GetPrivateUserDTO>> CreateNewPrivateUserAsync(PrivateUserDTO pUserDTO) {
+        public async Task<Response<GetPrivateUserDTO>> CreateNewPrivateUserAsync(PrivateUserDTO pUserDTO)
+        {
             var userType = await _context.UserTypes.Where(t => t.Type == pUserDTO.UserType).FirstOrDefaultAsync();
             var userStatus = await _context.UserStatuses.Where(s => s.Status == pUserDTO.UserStatus).FirstOrDefaultAsync();
             var address = await _context.Addresses.Where(a => a.Address1 == pUserDTO.Address.Address).FirstOrDefaultAsync();
@@ -142,11 +146,13 @@ namespace kontacto_api.Services
             var maxDaysDifference = 3650;
             var daysDifference = actualDate.Subtract(date).TotalDays;
 
-            if (userNameExist != null){
+            if (userNameExist != null)
+            {
                 return new Response<GetPrivateUserDTO>("Username exist", ResponseCodeEnum.FAILED);
-            } 
-            
-            if (userPrincipalEmailExist != null) { 
+            }
+
+            if (userPrincipalEmailExist != null)
+            {
                 return new Response<GetPrivateUserDTO>("Email exist", ResponseCodeEnum.FAILED);
             }
 
@@ -179,7 +185,8 @@ namespace kontacto_api.Services
 
             user = await this.CreateNewUserAsync(user);
 
-            var pUser = new PrivateUser {
+            var pUser = new PrivateUser
+            {
                 UserId = user.Id,
                 FirstName = pUserDTO.FirstName,
                 SecondName = pUserDTO.SecondName,
@@ -195,10 +202,9 @@ namespace kontacto_api.Services
             await _context.SaveChangesAsync();
             var getPrivateUser = await this.GetPrivateUserDTOAsync(pUser.UserId);
             return new Response<GetPrivateUserDTO>("User registered successfully!", ResponseCodeEnum.SUCCESSED, getPrivateUser);
-
-
         }
-        public async Task<Response<GetBusinessUserDTO>> CreateNewBusinessUserAsync(BusinessUserDTO bUserDTO) {
+        public async Task<Response<GetBusinessUserDTO>> CreateNewBusinessUserAsync(BusinessUserDTO bUserDTO)
+        {
             var userType = await _context.UserTypes.Where(t => t.Type == bUserDTO.UserType).FirstOrDefaultAsync();
             var userStatus = await _context.UserStatuses.Where(s => s.Status == bUserDTO.UserStatus).FirstOrDefaultAsync();
             var address = await _context.Addresses.Where(a => a.Address1 == bUserDTO.Address.Address).FirstOrDefaultAsync();
@@ -249,7 +255,8 @@ namespace kontacto_api.Services
 
             user = await this.CreateNewUserAsync(user);
 
-            var bUser = new BusinessUser {
+            var bUser = new BusinessUser
+            {
                 UserId = user.Id,
                 Name = bUserDTO.Name,
                 AnniversaryDate = DateTime.Parse(bUserDTO.AnniversaryDate)
